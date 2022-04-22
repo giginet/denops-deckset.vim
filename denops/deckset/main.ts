053ebb5ceec3db5378daa8f8d0432907e941f42e
giginet <giginet.net@gmail.com>
2022-04-22 21:59:46 +0900

    Implement traversing code blocks

--- denops/deckset/main.ts
+++ denops/deckset/main.ts
@@ -5,15 +5,82 @@ export async function main(denops: Denops): Promise<void> {
 
   denops.dispatcher = {
     async insertCodeHighlight(pos: number): Promise<void> {
-
+      const text = await getWholeText(denops);
+      const blocks = traverseAllCodeBlocks(text);
+      console.log(blocks[0].contents);
     }
   };
   await denops.cmd(`command! IH call denops#request('${denops.name}', 'insertCodeHighlight', [])`);
 };
 
+type Line = number;
+
 interface FencedCodeBlock {
-  start: number;
-  end: number;
+  start: Line;
+  end: Line;
   language?: string;
-  content: string;
+  contents: string[];
+}
+
+interface Range {
+  start: Line,
+  end: Line,
+}
+
+type HighlightedPosition = Line | Range | Array<Line>
+
+function traverseAllCodeBlocks(lines: string[]): FencedCodeBlock[] {
+  let codeBlocks: FencedCodeBlock[] = [];
+  const startPattern = /```(.+)?/;
+  const endPattern = /```/;
+
+  interface CurrentCodeBlock {
+    start: Line;
+    language?: string;
+    contents: string[];
+  }
+
+  let currentCodeBlock: CurrentCodeBlock = null;
+  lines.forEach((text, index) => { 
+    let startMatch = text.match(startPattern);
+    if (currentCodeBlock == null && startMatch) {
+      currentCodeBlock = {start: index + 1, language: startMatch[1], contents: []};
+    } else if (currentCodeBlock != null && text.match(endPattern)) {
+      let codeBlock: FencedCodeBlock = {
+        start: currentCodeBlock.start,
+        end: index + 1,
+        language: currentCodeBlock.language,
+        contents: currentCodeBlock.contents,
+      }
+      currentCodeBlock = null;
+      codeBlocks.push(codeBlock);
+    } else if (currentCodeBlock != null) {
+      currentCodeBlock.contents.push(text); 
+    }
+  });
+  return codeBlocks;
+}
+
+async function getWholeText(denops: Denops): Promise<string[]> {
+  const lines = await denops.call("getline", 1, "$");
+  return lines;
+}
+
+async function getCurrentCodeBlock(): Promise<FencedCodeBlock> {
+  return null;
+}
+
+function buildTag(pos: HighlightedPosition): string {
+  return `[.code-highlight: 1]`;
+}
+
+function insertTag(block: FencedCodeBlock, pos: HighlightedPosition): Promise<void> {
+  const tag = buildTag(pos);
+  let startLine = block.start;
+  let beforeLine = startLine - 1;
+  // await insert();
+}
+
+function convertGlobalToCodeBlockLine(line: number, codeBlock: FencedCodeBlock): number {
+  return line - codeBlock.start;
 }
